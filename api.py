8a864b46d57253398572f5759019f1ce1ed910ee
LeDocteurDesBits <77270859+LeDocteurDesBits@users.noreply.github.com>
2021-03-02 19:25:20 +0100

    HTTP error 400 now returns the API error

--- api.py
+++ api.py
@@ -43,7 +43,7 @@ class SmershAPI:
             raise requests.HTTPError('Resource not found', response=response)
 
         if response.status_code == 400:
-            raise requests.HTTPError('working as designed')
+            raise requests.HTTPError('Error 400: {}'.format(response.json()['hydra:description']))
 
         if response.status_code >= 500:
             raise requests.HTTPError('Well, I guess the server died ¯\\_(ツ)_/¯', response=response)
